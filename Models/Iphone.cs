b101fcebc4722cd6d02ca891ed78089a7f3a02aa
Wesley Peres <87404298+Wesll01@users.noreply.github.com>
2024-01-04 23:07:14 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string marca) : base(numero, modelo, marca)
+        {
+            // Construtor da classe base (Smartphone) é chamado para definir o número, modelo e marca
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na loja de aplicativos da Apple Store...");
+            // Lógica específica para instalar um aplicativo em um iPhone
+        }
     }
 }
