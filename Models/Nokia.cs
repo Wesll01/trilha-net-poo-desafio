f43d1ed491698226f64de4257bbaddaf6c14df5e
Wesley Peres <87404298+Wesll01@users.noreply.github.com>
2024-01-04 23:07:33 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string marca) : base(numero, modelo, marca)
+        {
+            // Construtor da classe base (Smartphone) é chamado para definir o número, modelo e marca
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na loja de aplicativos da Nokia...");
+            // Lógica específica para instalar um aplicativo em um dispositivo Nokia
+        }
     }
 }
