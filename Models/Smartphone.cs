86599727385c3523c49a6b3e522b156837aee34a
Wesley Peres <87404298+Wesll01@users.noreply.github.com>
2024-01-04 23:07:54 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; } // Propriedade para o modelo do smartphone
+        public string Marca { get; set; } // Propriedade para a marca do smartphone
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string marca)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Marca = marca;
         }
 
         public void Ligar()
